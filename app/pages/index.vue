68e312ea4567c1a4aa353f2204fbbbaa89e52a74
tsucharoku <rokuta_okanishi@66design.jp>
2026-04-05 16:39:42 +0900

    トップページ

---
+++ app/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <img :src="response.details.ext_1.url" width="800">
+        <div>{{ response.details.ext_2 }}</div>
+        <div>{{ response.details.ext_3 }}</div>
+
+        <div v-for="n in response.details.ext_4" :key="n.slag">
+            <img :src="n.ext_4.url" width="400">
+            <div>{{ n.ext_3 }}</div>
+            <div>{{ n.ext_5 }}</div>
+            <div>{{ n.ext_6 }}</div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const config = useRuntimeConfig();
+
+const { data: response } = await useFetch(
+    `${config.public.apiBase}/rcms-api/2/news/3`,
+    {
+        credentials: 'include',
+    }
+);
+</script>

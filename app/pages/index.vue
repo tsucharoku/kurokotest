0a700fdeab3152b45b93211fab4ab44e804502c2
tsucharoku <rokuta_okanishi@66design.jp>
2026-04-05 16:49:17 +0900

    news test

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,15 +1,6 @@
 <template>
     <div>
-        <img :src="response.details.ext_1.url" width="800">
-        <div>{{ response.details.ext_2 }}</div>
-        <div>{{ response.details.ext_3 }}</div>
-
-        <div v-for="n in response.details.ext_4" :key="n.slag">
-            <img :src="n.ext_4.url" width="400">
-            <div>{{ n.ext_3 }}</div>
-            <div>{{ n.ext_5 }}</div>
-            <div>{{ n.ext_6 }}</div>
-        </div>
+        <div>{{ response.details.subject }}</div>
     </div>
 </template>
 
@@ -17,7 +8,7 @@
 const config = useRuntimeConfig();
 
 const { data: response } = await useFetch(
-    `${config.public.apiBase}/rcms-api/2/news/3`,
+    `${config.public.apiBase}rcms-api/2/news/3`,
     {
         credentials: 'include',
     }
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
       // Public keys that are exposed to the client
       public: {
-          apiBase: 'https://okaroku2.g.kuroco-mng.app/'
+          apiBase: 'https://okaroku2.g.kuroco.app/'
       }
   },
 })

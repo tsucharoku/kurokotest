a0afdc9c8f466e2b0ed8201ba6f40934e9545d51
tsucharoku <rokuta_okanishi@66design.jp>
2026-04-05 16:06:17 +0900

    設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  ssr: false,
+  runtimeConfig: {
+      // Public keys that are exposed to the client
+      public: {
+          apiBase: 'https://okaroku2.g.kuroco-mng.app/'
+      }
+  },
 })
